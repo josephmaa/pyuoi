335397aa960b7e8747cf21101cd1d2a2fb04cc31
Joseph Maa <josephgmaa@berkeley.edu>
2022-09-13 20:13:19 -0700

    Compile dataset frame/behaviors + behavior counts

--- classifier/compile_dataset.py
+++ classifier/compile_dataset.py
@@ -3,9 +3,12 @@ import argparse
 import os
 import pandas as pd
 import xarray as xr
-
+import datetime
 from collections import Counter
+
+from hashlib import sha256
 from visualize_dataset_distribution import Dataset
+from logging.log_writer import DatasetLog
 
 
 def initialize_arg_parser():
@@ -17,14 +20,9 @@ def initialize_arg_parser():
         default="features.all",
     )
     parser.add_argument(
-        "--output_files",
-        help="Optional argument for the output directory of the files",
-        default="",
-    )
-    parser.add_argument(
-        "--aggregate",
-        help="Speed up computations by loading behaviors into dict instead.",
-        default=False,
+        "--output_directory",
+        help="Argument for the output directory of the files",
+        default=os.getcwd(),
     )
     return parser
 
@@ -36,27 +34,36 @@ def main():
     df = pd.DataFrame()
     num_files = len(os.listdir(ps.input_directory))
 
-    if ps.aggregate:
-        counts = Counter()
+    time = str(datetime.datetime.now().strftime("%Y_%m_%d_%I_%M_%S"))
+    behavior_output_directory = os.path.join(ps.output_directory, time, "behaviors")
+    counts_output_directory = os.path.join(ps.output_directory, time, "counts")
+
+    print(behavior_output_directory
+    )
+    os.makedirs(behavior_output_directory, exist_ok=True)
+    os.makedirs(counts_output_directory, exist_ok=True)
 
     for i, input_file in enumerate(os.listdir(ps.input_directory)):
         print(f"Processing {i} of {num_files}")
-        print(counts)
         if input_file.endswith(".netcdf"):
-            new_dataframe = xr.load_dataset(
+            hash_filename = f"{sha256(input_file.encode('utf-8')).hexdigest()}.txt"
+
+            # Get the frame and behavior name from the dataframe
+            behaviors_dataframe = xr.load_dataset(
                 os.path.join(os.getcwd(), "features.all", input_file),
                 engine="h5netcdf",
-            ).to_dataframe()
-            if ps.aggregate:
-                counts.update(Counter(new_dataframe["behavior_name"]))
-            else:
-                df = pd.concat([df, new_dataframe])
+            ).to_dataframe()[["vid_framenum", "behavior_name"]]
+            dataset_behaviors_log = DatasetLog(data = behaviors_dataframe)
 
-    ds.initialize_dataset(df)
-    ds.distribution(output_file=ps.output_files)
+            dataset_behaviors_log.write(behavior_output_directory, hash_filename)
+            
+            # Get the counts of the behaviors
+            counts = Counter(behaviors_dataframe["behavior_name"])
+            counts_dataframe = pd.DataFrame.from_records(list(counts.items()), columns=['behavior_name','count'])
+            counts_dataframe.sort_values(by=["behavior_name"], inplace=True)
+            dataset_counts_log = DatasetLog(data = counts_dataframe)
+            dataset_counts_log.write(counts_output_directory, hash_filename)
 
-    if ps.aggregate:
-        print(counts)
 
 
 if __name__ == "__main__":
