234831657c48da4ebee3df95b2a669803d6bd68d
josephgmaa <josephgmaa@berkeley.edu>
2022-11-08 14:06:19 -0800

    Update create classifier

--- create_classifier.py
+++ create_classifier.py
@@ -7,39 +7,42 @@ import numpy as np
 from sklearn.metrics import accuracy_score
 
 
-
-DB_FILE_PATH = "/wynton/home/manoli/josephgmaa/pyuoi/test_small.db"
+# DB_FILE_PATH = "/wynton/home/manoli/josephgmaa/pyuoi/test_small.db"
+DB_FILE_PATH = "/wynton/home/manoli/josephgmaa/pyuoi/test.db"
 
 
 def main():
-	conn = sqlite3.connect(DB_FILE_PATH)
-	df = pd.read_sql(sql="SELECT * FROM features_small", con=conn)
+    conn = sqlite3.connect(DB_FILE_PATH)
+    df = pd.read_sql(sql="SELECT * FROM features", con=conn)
 
-	y = df["behavior_name"].to_numpy()
-	le = LabelEncoder()
-	le.fit(y)
+    y = df["behavior_name"].to_numpy()
+    le = LabelEncoder()
+    le.fit(y)
 
-	x_train, x_test, y_train, y_test = train_test_split(
-				df.loc[:, df.columns != "behavior_name"].to_numpy(),
-				le.transform(y),
-				random_state=1,
-			)
-	l1log = UoI_L1Logistic(random_state=1, multi_class="multinomial").fit(x_train, y_train, verbose=True)
+    # We only use dtypes of float64 in training for columns.
+    x_train, x_test, y_train, y_test = train_test_split(
+        df.loc[:, df.dtypes == np.float64].to_numpy(),
+        le.transform(y),
+        random_state=1,
+    )
 
-	y_hat = l1log.predict(x_test)
+    l1log = UoI_L1Logistic(random_state=1, multi_class="multinomial").fit(
+        x_train, y_train, verbose=True
+    )
 
-	accuracy = accuracy_score(y_test, y_hat)
-	print("y_test: ", y_test)
-	print("y_hat: ", y_hat)
-	y_test_freq = np.bincount(y_test)
-	y_hat_freq = np.bincount(y_hat)
-	print("y_test_freq: ", y_test_freq)
-	print("y_hat_freq: ", y_hat_freq)
+    y_hat = l1log.predict(x_test)
 
-	print(f"Accuracy: {accuracy}")
-	print(f"Resulting values: {y_hat}")
-	
+    accuracy = accuracy_score(y_test, y_hat)
+    print("y_test: ", y_test)
+    print("y_hat: ", y_hat)
+    y_test_freq = np.bincount(y_test)
+    y_hat_freq = np.bincount(y_hat)
+    print("y_test_freq: ", y_test_freq)
+    print("y_hat_freq: ", y_hat_freq)
+
+    print(f"Accuracy: {accuracy}")
+    print(f"Resulting values: {y_hat}")
 
 
 if __name__ == "__main__":
-	main()
+    main()
