94afbc2b97df76ab5905fcc98b37ffb002009d47
josephgmaa <josephgmaa@berkeley.edu>
2022-12-13 15:32:25 -0800

    Update create classifier

--- create_classifier.py
+++ create_classifier.py
@@ -5,26 +5,41 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
 from sklearn.metrics import accuracy_score
+import argparse
+from sklearn.preprocessing import Normalizer
 
 
-# DB_FILE_PATH = "/wynton/home/manoli/josephgmaa/pyuoi/test_small.db"
-DB_FILE_PATH = "/wynton/home/manoli/josephgmaa/pyuoi/test.db"
-
-
-def main():
-    conn = sqlite3.connect(DB_FILE_PATH)
+def main(args):
+    conn = sqlite3.connect(args.database_path)
     df = pd.read_sql(sql="SELECT * FROM features", con=conn)
 
     y = df["behavior_name"].to_numpy()
     le = LabelEncoder()
     le.fit(y)
 
+    X = df.loc[:, df.dtypes == np.float64].to_numpy()
+    transformer = Normalizer()
+    input_data = transformer.transform(X)
+
     # We only use dtypes of float64 in training for columns.
     x_train, x_test, y_train, y_test = train_test_split(
-        df.loc[:, df.dtypes == np.float64].to_numpy(),
+        input_data,
         le.transform(y),
         random_state=1,
     )
+
+    print(
+        f"label encoder output {le.transform(y).shape}, actual output {le.transform(y)}"
+    )
+
+    pd.set_option("display.max_rows", None, "display.max_columns", None)
+    print(
+        f"arrays: {df.loc[0, df.dtypes == np.float64]}, shape: {df.loc[:, df.dtypes == np.float64].shape}"
+    )
+
+    print(f"x train: {x_train}, x_test: {x_test}")
+
+    print(f"y test: {y_test}, y_train: {y_train}")
 
     l1log = UoI_L1Logistic(random_state=1, multi_class="multinomial").fit(
         x_train, y_train, verbose=True
@@ -42,7 +57,12 @@ def main():
 
     print(f"Accuracy: {accuracy}")
     print(f"Resulting values: {y_hat}")
+    print(f"selection values: {l1log._selection_lm}")
+    print(f"estimation values: {l1log._estimation_lm}")
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--database_path", help="path to the database")
+    args = parser.parse_args()
+    main(args)
