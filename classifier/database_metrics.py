f7da5de432bbf0761cad257c17a695fe6f69532f
josephgmaa <josephgmaa@berkeley.edu>
2023-01-10 16:29:25 -0800

    Add checking for database distribution

---
+++ classifier/database_metrics.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+
+def generate_sql_database_distribution(path_to_database: str):
+    """
+    Count all the unique behaviors in the column 'behavior' in the given sql database.
+
+    Input:
+        path_to_database: path to database as a string.
+    Output:
+        behavior_map: dictionary of behaviors to count.
+    """
+    # Connect the existing database to the file.
+    conn = sqlite3.connect(path_to_database)
+    cur = conn.cursor()
+    cur.execute(f"SELECT behavior_name, COUNT(*) FROM features GROUP BY behavior_name")
+
+    result = cur.fetchall()
+    conn.close()
+
+    return result
